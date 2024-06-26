8d6d417f96d1b0739a99567e8fe9237c27799a1b
Saamu02 <iusamairfan@gmail.com>
2024-06-26 13:58:35 +0500

    Project created

---
+++ Africa/Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Ussama Irfan on 26/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
