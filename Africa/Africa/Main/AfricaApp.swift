7934a68999d014b2eb20e2ae47d212ef9ea5c00a
Saamu02 <iusamairfan@gmail.com>
2024-06-26 15:24:09 +0500

    Added assets to the project and setup launch screen

---
+++ Africa/Africa/Main/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ussama Irfan on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
